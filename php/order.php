a16c52a3f7707844a5431ebf859999e1e374dc39
Paurnima Partole <partolepaurnima8@gmail.com>
2018-03-11 15:01:40 +0530

    modified the order.php file to get proper object structure

--- php/order.php
+++ php/order.php
@@ -1,9 +1,17 @@
 <?php
 
 $result = json_encode($_POST);
-$file = fopen("order.json","a");
-// $result = '' $result . '],';
-fwrite($file, $result);
 
-
+$str = file_get_contents('order.json');
+$old = '{"pizza":[';
+$old2 = ']}';
+$new = '';
+$new2 = '';
+$arrayBraces = '';
+$st = str_replace($old, $new, $str);
+$st = str_replace($old2, $new2, $st);
+$result = $st . $result;
+$arrayBraces = '{"pizza":[' . $result . ']}';
+$arrayBraces = str_replace("}{", "},{", $arrayBraces);
+file_put_contents('order.json', $arrayBraces);
 ?>
